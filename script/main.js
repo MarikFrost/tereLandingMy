11ca6171ff08a1111fa4c514100e8be7c66c1568
unknown <recruiterhrpartner4@gmail.com>
2025-08-23 16:44:13 +0300

    додав інтерактивне бургер меню, ефекти натиску на кнопки

---
+++ script/main.js
@@ -0,0 +1,64 @@
+const burger = document.querySelector('.burger-menu')
+const mobileMenu = document.querySelector('.wrap-navigation')
+/**************************Бургер ********************/
+document.addEventListener('click', (event) => {
+    if(event.target.closest('.burger-menu') === burger) {
+        burger.classList.toggle('rotate')
+        mobileMenu.classList.toggle('visible')
+
+        const smallBurger = () => {
+            burger.classList.remove('rotate')
+            burger.removeEventListener('transitionend', smallBurger)
+        }
+        burger.addEventListener('transitionend', smallBurger)
+
+    } else if (!event.target.closest('.wrap-navigation')) {
+        burger.classList.remove('rotate')
+        mobileMenu.classList.remove('visible')
+    }
+})
+
+/***************************Тень на кнопке *****************/
+let helpButton = document.querySelector('.wrap-navigation > a')
+
+helpButton.addEventListener('pointerdown', () => {
+    helpButton.classList.add('button')
+})
+helpButton.addEventListener('pointerup', () => {
+    helpButton.classList.remove('button')
+})
+helpButton.addEventListener('pointerleave', () => {
+        helpButton.classList.remove('button')
+    })
+/********************Кнопки завантаження *************** */
+let download = document.querySelectorAll('.download-block> a')
+
+download.forEach(el => {
+    el.addEventListener('pointerdown', () => {
+        el.classList.add('download-block-shadow')
+    })
+    el.addEventListener('pointerup', () => {
+        el.classList.remove('download-block-shadow')
+    })
+    el.addEventListener('pointerleave', () => {
+        el.classList.remove('download-block-shadow')
+    })
+})
+
+/******************APPLY TO DRIVE****************/
+const applyToDrive = document.querySelector('.applyToDrive input')
+const applyTobutton = document.querySelector('.applyToDrive button')
+
+applyToDrive.addEventListener('input', () => {
+    if(applyToDrive.value.length !== 0) {
+        applyTobutton.style.backgroundColor = '#000000'
+        applyTobutton.style.cursor = 'pointer'
+        applyTobutton.disabled = false;
+    } else {
+        applyTobutton.style.cursor = 'not-allowed'
+        applyTobutton.style.backgroundColor = '#4d4848'
+        applyTobutton.disabled = true;
+    }
+})
+
+/***********************СЛАЙДЕР**************************** */
