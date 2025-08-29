bd7b2cbae6866a3510df2fd75b3124fb680d61ff
unknown <recruiterhrpartner4@gmail.com>
2025-08-29 11:08:40 +0300

    додав кастомний слайдер з перемиканням по булiтам

--- script/main.js
+++ script/main.js
@@ -56,9 +56,117 @@ applyToDrive.addEventListener('input', () => {
         applyTobutton.disabled = false;
     } else {
         applyTobutton.style.cursor = 'not-allowed'
-        applyTobutton.style.backgroundColor = '#4d4848'
+        applyTobutton.style.backgroundColor = 'rgb(169 169 169)'
         applyTobutton.disabled = true;
     }
 })
 
+
 /***********************СЛАЙДЕР**************************** */
+/*Автоматичне перемикання слайдів*/
+const autoSlider = (ObjWithHtmlElSlider, animationTime, index) => {
+    let itemWidth = ObjWithHtmlElSlider.children[index].getBoundingClientRect().width
+    offset = -(itemWidth + gap) * (index)
+    ObjWithHtmlElSlider.style.transition = `transform ${animationTime}s ease-in-out`
+    ObjWithHtmlElSlider.style.transform = `translateX(${offset}px)`
+}
+/*Автоматичне перемикання слайдів*/
+/* РУЧНЕ ЗАФАРБОВУВАННЯ КЛАСІВ */
+const handClass = (ObjWithHtmlElCount, interval) => {
+    Array.from(ObjWithHtmlElCount.children).forEach((el, index) => {
+        el.addEventListener('click', event => {
+            if(index > slide) {
+                box = false
+                count = index
+                handClone(index, slide)
+                slide = index + 1
+                autoClass(ObjWithHtmlElCount, index)
+                autoSlider(sliderBlock, 0.2, index)
+            } else {
+                slide = index
+                count = index
+                autoClass(ObjWithHtmlElCount, count)
+                autoSlider(sliderBlock, 0.2, slide)
+                if(sliderBlock.children.length >= lengthSlider) {
+                    for(let i = sliderBlock.children.length - 1; i > lengthSlider + index; i--) {
+                        sliderBlock.children[i].remove()
+                    }
+                }
+            }
+            
+
+            setTimeout(()=>{
+                box = true
+            }, interval)
+        })
+    })
+    
+}
+/* РУЧНЕ ЗАФАРБОВУВАННЯ КЛАСІВ */
+/*АВТОМИТИЧНЕ ЗАФАРБОВУВАННЯ ПЕРЕМИКАЧІВ*/
+const autoClass = (ObjWithHtmlElCount, index) => {
+    if(index >= ObjWithHtmlElCount.children.length) {
+        count = 0
+        index = count
+    }
+    Array.from(ObjWithHtmlElCount.children).forEach(el => {
+        el.classList.remove('active')
+    })
+    ObjWithHtmlElCount.children[index].classList.add('active')
+}
+/*АВТОМИТИЧНЕ ЗАФАРБОВУВАННЯ ПЕРЕМИКАЧІВ*/
+/* АВТОМАТИЧНЕ ВИДАЛЕННЯ СЛАЙДІВ */
+const andSlider = (ObjWithHtmlElSlider) => {
+    const listener = () => {
+        for(let i = 0; i <= lengthSlider; i++){
+            ObjWithHtmlElSlider.children[0].remove()
+        }
+        ObjWithHtmlElSlider.style.transition = 'transform 0s ease-in-out'
+        ObjWithHtmlElSlider.style.transform = `translateX(0px)`
+        slide = 0
+
+        ObjWithHtmlElSlider.removeEventListener('transitionend', listener)
+    }
+    ObjWithHtmlElSlider.addEventListener('transitionend', listener)
+}
+/* АВТОМАТИЧНЕ ВИДАЛЕННЯ СЛАЙДІВ */
+const handClone = (index, slide) => {
+    for(slide; slide <= index; slide++){
+        const itemSlider = sliderBlock.children[slide].cloneNode(true)
+        sliderBlock.append(itemSlider)
+    }
+}
+
+
+const sliderBlock = document.querySelector('.works-slider > div')
+const countBlock = document.querySelector('.count-slider')
+let slide = 0
+let count = 0
+let offset = 0
+let box = true
+let gap = parseInt(window.getComputedStyle(sliderBlock).gap)
+    if(isNaN(gap)) {
+        gap = 0
+    }
+let lengthSlider = sliderBlock.children.length - 1
+
+const autoAndHandSlide = (sliderBlock, countBlock, interval, animationTime) => {
+    interval = interval * 1000
+    handClass(countBlock, interval)
+    setInterval(() => {
+    if(box) {
+            const itemSlider = sliderBlock.children[slide].cloneNode(true)
+            sliderBlock.append(itemSlider)
+            slide++
+            count++
+            autoSlider(sliderBlock, animationTime, slide)
+            autoClass(countBlock, count)
+            if(slide > lengthSlider) {
+                andSlider(sliderBlock)
+            } 
+        }  
+    }, interval) 
+}
+autoAndHandSlide(sliderBlock, countBlock, 4, 1)
+
+
